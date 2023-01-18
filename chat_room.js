196d5141896d91e1dd3f37ebdde38e787704c10e
0livia123 <102658551+0livia123@users.noreply.github.com>
2023-01-18 17:13:46 +0800

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA8Ec_HtTxiui0oj6tFWiZ50RGuJpMLaL8",
+    authDomain: "let-s-chat-df3cd.firebaseapp.com",
+    databaseURL: "https://let-s-chat-df3cd-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-df3cd",
+    storageBucket: "let-s-chat-df3cd.appspot.com",
+    messagingSenderId: "135702983045",
+    appId: "1:135702983045:web:190e41ffe72530787f54ec"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
